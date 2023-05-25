6b6f8220b1fadb3be5405c77de117f85dca9b628
logoutbye <muhammadbilal03478@gmail.com>
2023-05-24 22:57:12 -0700

    pushing hey jinni lite in new repo

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,127 +1,189 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:ezeehome_webview/Contrlller/InternetConnectivity.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_pro/webview_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Home extends StatefulWidget {
   Home({
     super.key,
     required this.isInternetConnected,
-  }) {}
+    // required this.interstitialAd01,
+    // required this.isloadedIntAd
+  }) {
+    if (this.isInternetConnected) {
+      initInterstitialAd();
+      Future.delayed(Duration(seconds: 4), () async {
+        if (this.isloadedIntAd == true) {
+          this.interstitialAd.show();
+        }
+      });
+    }
+  }
 
   bool isInternetConnected;
+  // InterstitialAd interstitialAd01;
+
+  late InterstitialAd interstitialAd;
+  bool isloadedIntAd = false;
+//bool IsInternetConnectedforAds = true;
+
+  initInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: 'ca-app-pub-4371054866340571/6418410324',
+      request: AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: onAdLoaded,
+        onAdFailedToLoad: (Error) {
+          print('My Eror  ////////////////=/////////////////  $Error');
+        },
+      ),
+    );
+  }
+
+  void onAdLoaded(InterstitialAd ad) {
+    interstitialAd = ad;
+    isloadedIntAd = true;
+
+    interstitialAd.fullScreenContentCallback = FullScreenContentCallback(
+      onAdDismissedFullScreenContent: (ad) {
+        interstitialAd.dispose();
+      },
+      onAdFailedToShowFullScreenContent: (ad, error) {
+        interstitialAd.dispose();
+      },
+    );
+  }
+
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  late InterstitialAd interstitialAd;
+  bool _isloadedIntAd = false;
+  //=================
   var IsInternetConnected = true;
   bool loader = false;
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
 
   @override
   void initState() {
+    // interstitialAd = widget.interstitialAd;
+    // _isloadedIntAd = widget._isloadedIntAd;
+    // TODO: implement initState
     checkInternetConnectionForDashboard();
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
+    if (IsInternetConnected) {
+      widget.initInterstitialAd();
+      Future.delayed(Duration(seconds: 4), () async {
+        if (widget.isloadedIntAd == true) {
+          widget.interstitialAd.show();
+        }
+      });
     }
-    requestPermissions();
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF04abf2),
-        // title: Text('DriveBC'),
-        //centerTitle: true,
-        toolbarHeight: 10,
-      ),
-      body: IsInternetConnected == false
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.signal_wifi_connected_no_internet_4,
-                    size: 60,
-                    color: Colors.black,
-                  ),
-                  Container(
-                    child: Text(
-                      'No Internet Connection',
-                      style: TextStyle(
-                        fontSize: 18,
+    // if (IsInternetConnected == true) {
+    widget.initInterstitialAd();
+    if (widget.isloadedIntAd == true) {
+      widget.interstitialAd.show();
+    }
+    //}
+
+    return WillPopScope(
+      onWillPop: () async {
+        bool? goBack = await controller.canGoBack();
+        if (goBack != true)
+          return true;
+        else {
+          controller.goBack();
+          return false;
+        }
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xFF34dce7),
+            // title: Text('DriveBC'),
+            //centerTitle: true,
+            toolbarHeight: 10,
+          ),
+          body: IsInternetConnected == false
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.signal_wifi_connected_no_internet_4,
+                        size: 60,
+                        color: Colors.black,
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF04abf2),
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => Home(
-                          isInternetConnected: IsInternetConnected,
+                      Container(
+                        child: Text(
+                          'No Internet Connection',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
                         ),
-                      ));
-                    },
-                    child: Text('Reload Page'),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 181, 12, 219),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Home(
+                              isInternetConnected: IsInternetConnected,
+                              // interstitialAd01: widget.interstitialAd01,
+                              // isloadedIntAd: widget.isloadedIntAd,
+                              // interstitialAd: interstitialAd,
+                              // isloadedIntAd: _isloadedIntAd,
+                            ),
+                          ));
+                        },
+                        child: Text('Reload Page'),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            )
-          : Builder(builder: (BuildContext context) {
-              return WebView(
-                initialUrl: 'https://heyjinni.com/',
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (WebViewController webViewController) {
-                  _controller.complete(webViewController);
-                },
-                onProgress: (int progress) {
-                  print("WebView is loading (progress : $progress%)");
-                },
-                javascriptChannels: <JavascriptChannel>{
-                  // _toasterJavascriptChannel(context),
-                  JavascriptChannel(
-                      name: 'Toaster',
-                      onMessageReceived: (JavascriptMessage message) {
-                        var snackBar = SnackBar(
-                          content: Text(message.message),
-                        );
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      })
-                },
-                navigationDelegate: (NavigationRequest request) {
-                  if (request.url.startsWith('https://www.youtube.com/')) {
-                    print('blocking navigation to $request}');
-                    return NavigationDecision.prevent;
-                  }
-                  print('allowing navigation to $request');
-                  return NavigationDecision.navigate;
-                },
-                onPageStarted: (String url) {
-                  print('Page started loading: $url');
-                },
-                onPageFinished: (String url) {
-                  print('Page finished loading: $url');
-                },
-                gestureNavigationEnabled: true,
-                geolocationEnabled: false, //support geolocation or not
-                zoomEnabled: true,
-              );
-            }),
+                )
+              : WebViewWidget(controller: controller)),
     );
   }
+
+//=============================================
+  var controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..reload()
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    // ..loadRequest(Uri.parse('https://cellphonestars.com/'));
+    ..loadRequest(Uri.parse('https://influencerswithlikes.com/'));
 
   checkInternetConnectionForDashboard() async {
     // to check the internet connection
@@ -139,26 +201,4 @@ class _HomeState extends State<Home> {
       });
     }
   }
-
-  void requestPermissions() async {
-    Map<Permission, PermissionStatus> statuses = await [
-      Permission.camera,
-      Permission.storage,
-      Permission.microphone,
-      Permission.phone,
-    ].request();
-
-    if (statuses[Permission.camera]!.isGranted &&
-        statuses[Permission.storage]!.isGranted &&
-        statuses[Permission.microphone]!.isGranted &&
-        statuses[Permission.phone]!.isGranted) {
-      // All permissions granted, proceed with the functionality.
-      print('All permissions granted!');
-    } else {
-      // Permissions not granted, handle accordingly.
-      print('Some or all permissions not granted!');
-    }
-  }
-
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:ezeehome_webview/Contrlller/InternetConnectivity.dart';
 import 'package:ezeehome_webview/Screens/Home.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   await checkInternetConnectionForDashboard();
+  // if (isInternetConnected) {
+  //   Future.delayed(Duration(seconds: 4), () async {
+  //     initInterstitialAd();
+  //   });
+  // }
+  // Future.delayed(Duration(seconds: 4), () async {
+  //   runApp(MyApp());
+  // });
   runApp(MyApp());
 }
 
@@ -22,6 +30,36 @@ checkInternetConnectionForDashboard() async {
   }
 }
 
+// late InterstitialAd interstitialAd;
+// bool _isloadedIntAd = false;
+
+// initInterstitialAd() {
+//   InterstitialAd.load(
+//     adUnitId: 'ca-app-pub-4371054866340571/4866124695',
+//     request: AdRequest(),
+//     adLoadCallback: InterstitialAdLoadCallback(
+//       onAdLoaded: onAdLoaded,
+//       onAdFailedToLoad: (Error) {
+//         print('My Eror  ////////////////=/////////////////  $Error');
+//       },
+//     ),
+//   );
+// }
+
+// void onAdLoaded(InterstitialAd ad) {
+//   interstitialAd = ad;
+//   _isloadedIntAd = true;
+
+//   interstitialAd.fullScreenContentCallback = FullScreenContentCallback(
+//     onAdDismissedFullScreenContent: (ad) {
+//       interstitialAd.dispose();
+//     },
+//     onAdFailedToShowFullScreenContent: (ad, error) {
+//       interstitialAd.dispose();
+//     },
+//   );
+// }
+
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
@@ -30,12 +68,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Hey Jinni Lite',
+        title: 'Influencers with Likes',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: Home(
           isInternetConnected: isInternetConnected,
+          // interstitialAd01: interstitialAd, isloadedIntAd: _isloadedIntAd,
+          // interstitialAd: interstitialAd,
+          // isloadedIntAd: _isloadedIntAd,
         ));
   }
 }
