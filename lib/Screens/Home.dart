662a252b0d05455ea7a886dd3ba95bf301132d21
Fahad Khan Yousafzai <pak.fahadullah@gmail.com>
2023-05-29 15:30:51 +0500

    fahad

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -28,8 +28,8 @@ class _HomeState extends State<Home> {
   final _webViewController = Completer<WebViewController>();
   double _progress = 0.0; // Variable to hold the progress percentage
   bool _isLoading = true;
-  
-  int _progressText =0; // Variable to track loading state
+
+  int _progressText = 0; // Variable to track loading state
 
   @override
   void initState() {
@@ -40,162 +40,171 @@ class _HomeState extends State<Home> {
     requestPermissions();
     super.initState();
   }
-@override
-Widget build(BuildContext context) {
-  return WillPopScope(
-    onWillPop: () async {
-      bool? goBack = await _webViewController.future
-          .then((value) => value.canGoBack());
-      if (goBack != true) {
-        return true;
-      } else {
-        _webViewController.future
-            .then((controller) => controller.goBack());
-        return false;
-      }
-    },
-    child: Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(0),
-        child: AppBar(
-          backgroundColor: MyColors.kprimaryColor,
-          elevation: 0,
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        bool? goBack =
+            await _webViewController.future.then((value) => value.canGoBack());
+        if (goBack != true) {
+          return true;
+        } else {
+          _webViewController.future.then((controller) => controller.goBack());
+          return false;
+        }
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(0),
+          child: AppBar(
+            backgroundColor: MyColors.kprimaryColor,
+            elevation: 0,
+          ),
         ),
-      ),
-      body: IsInternetConnected == false
-          ? RefreshIndicator(
-              onRefresh: () {
-                return Future.delayed(Duration(seconds: 1), () {
-                  Navigator.pop(context);
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Home(
-                      isInternetConnected: IsInternetConnected,
-                    ),
-                  ));
-                });
-              },
-              child: SingleChildScrollView(
-                physics: AlwaysScrollableScrollPhysics(),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.signal_wifi_connected_no_internet_4,
-                          size: 60,
-                          color: MyColors.kprimaryColor,
-                        ),
-                        Container(
-                          child: Text(
-                            'No Internet Connection',
-                            style: TextStyle(
-                              fontSize: 18,
+        body: IsInternetConnected == false
+            ? RefreshIndicator(
+                onRefresh: () {
+                  return Future.delayed(Duration(seconds: 1), () {
+                    Navigator.pop(context);
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => Home(
+                        isInternetConnected: IsInternetConnected,
+                      ),
+                    ));
+                  });
+                },
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.signal_wifi_connected_no_internet_4,
+                            size: 60,
+                            color: MyColors.kprimaryColor,
+                          ),
+                          Container(
+                            child: Text(
+                              'No Internet Connection',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: MyColors.kprimaryColor,
+                          SizedBox(
+                            height: 20,
                           ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                            Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => Home(
-                                isInternetConnected: IsInternetConnected,
-                              ),
-                            ));
-                          },
-                          child: Text('Reload Page'),
-                        ),
-                      ],
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: MyColors.kprimaryColor,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => Home(
+                                  isInternetConnected: IsInternetConnected,
+                                ),
+                              ));
+                            },
+                            child: Text('Reload Page'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          : Stack(
-              children: [
-                WebView(
-                  initialUrl: 'https://heyjinni.com/',
-                  javascriptMode: JavascriptMode.unrestricted,
-                  onWebViewCreated: (WebViewController webViewController) {
-                    _webViewController.complete(webViewController);
-                  },  javascriptChannels: <JavascriptChannel>{
-                          // _toasterJavascriptChannel(context),
-                          JavascriptChannel(
-                              name: 'Toaster',
-                              onMessageReceived: (JavascriptMessage message) {
-                                var snackBar = SnackBar(
-                                  content: Text(message.message),
-                                );
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(snackBar);
-                              })
-                        },
-                        navigationDelegate: (NavigationRequest request) {
-                          if (request.url.startsWith('https://heyjinni')) {
-                            return NavigationDecision.navigate;
-                          } else if (request.url
-                              .startsWith('https://www.youtube.com/')) {
-                            print('blocking navigation to $request}');
-                            return NavigationDecision.prevent;
-                          } else {
-                            print('opening external link');
-                            _launchExternalUrl(request.url);
-                            // launchUrl(Uri.parse(request.url));
-                          }
-                          print('allowing navigation to $request');
-                          return NavigationDecision.navigate;
-                        },
-                  onProgress: (int progress) {
-                    print("WebView is loading (progress : $progress%)");
-                    setState(() {
-                      _progress = progress / 100; 
-                      _progressText = progress;
-                      // Update progress based on the value received (0-100)
-                    });
-                  },
-                  onPageStarted: (String url) {
-                    print('Page started loading: $url');
-                    setState(() {
-                      _isLoading = true; // Set loading state to true when a new page starts loading
-                    });
-                  },
-                  onPageFinished: (String url) {
-                    print('Page finished loading: $url');
-                    setState(() {
-                      _isLoading = false; // Set loading state to false when the page finishes loading
-                    });
-                  },
-                  gestureNavigationEnabled: true,
-                  geolocationEnabled: false,
-                  zoomEnabled: true,
-                ),
-                Visibility(
-                  visible: _isLoading, // Show the progress indicator only when loading
-                  child:Center(
-                    child: CircularPercentIndicator(
-                          radius: 80.0,
-                          lineWidth: 15.0,
-                          percent: _progress,
-                          center: new Text("$_progressText%",style: TextStyle(color: MyColors.kprimaryColor,fontSize: 40),),
-                          progressColor: MyColors.kprimaryColor,
-                          backgroundColor: Color.fromARGB(255, 104, 204, 247),
-                          circularStrokeCap: CircularStrokeCap.round,
-                        ),
+              )
+            : Stack(
+                children: [
+                  WebView(
+                    initialUrl: 'https://heyjinni.com/',
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (WebViewController webViewController) {
+                      _webViewController.complete(webViewController);
+                    },
+                    javascriptChannels: <JavascriptChannel>{
+                      // _toasterJavascriptChannel(context),
+                      JavascriptChannel(
+                          name: 'Toaster',
+                          onMessageReceived: (JavascriptMessage message) {
+                            var snackBar = SnackBar(
+                              content: Text(message.message),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          })
+                    },
+                    navigationDelegate: (NavigationRequest request) {
+                      if (request.url.startsWith('https://heyjinni.com/')) {
+                        return NavigationDecision.navigate;
+                      } else if (request.url
+                          .startsWith('https://www.youtube.com/')) {
+                        print('blocking navigation to $request}');
+                        return NavigationDecision.prevent;
+                      } else {
+                        print('opening external link');
+                        _launchExternalUrl(request.url);
+                        // launchUrl(Uri.parse(request.url));
+                        return NavigationDecision.prevent;
+                      }
+                      print('allowing navigation to $request');
+                      // return NavigationDecision.navigate;
+                    },
+                    onProgress: (int progress) {
+                      print("WebView is loading (progress : $progress%)");
+                      setState(() {
+                        _progress = progress / 100;
+                        _progressText = progress;
+                        // Update progress based on the value received (0-100)
+                      });
+                    },
+                    onPageStarted: (String url) {
+                      print('Page started loading: $url');
+                      setState(() {
+                        _isLoading =
+                            true; // Set loading state to true when a new page starts loading
+                      });
+                    },
+                    onPageFinished: (String url) {
+                      print('Page finished loading: $url');
+                      setState(() {
+                        _isLoading =
+                            false; // Set loading state to false when the page finishes loading
+                      });
+                    },
+                    gestureNavigationEnabled: true,
+                    geolocationEnabled: false,
+                    zoomEnabled: true,
                   ),
-                  //  CircularProgressIndicator(value: _progress),
-                ),
-              ],
-            ),
-    ),
-  );
-}
+                  Visibility(
+                    visible:
+                        _isLoading, // Show the progress indicator only when loading
+                    child: Center(
+                      child: CircularPercentIndicator(
+                        radius: 80.0,
+                        lineWidth: 15.0,
+                        percent: _progress,
+                        center: new Text(
+                          "$_progressText%",
+                          style: TextStyle(
+                              color: MyColors.kprimaryColor, fontSize: 40),
+                        ),
+                        progressColor: MyColors.kprimaryColor,
+                        backgroundColor: Color.fromARGB(255, 104, 204, 247),
+                        circularStrokeCap: CircularStrokeCap.round,
+                      ),
+                    ),
+                    //  CircularProgressIndicator(value: _progress),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
 
   // @override
   // Widget build(BuildContext context) {
@@ -360,6 +369,7 @@ Widget build(BuildContext context) {
       throw 'Could not launch $url';
     }
   }
+
   checkInternetConnectionForDashboard() async {
     // to check the internet connection
     await CheckInternetConnection.checkInternetFunction();
